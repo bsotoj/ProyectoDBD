e10b573dd0270ca05a6ddc819e00e5e9404d904d
Bastian Soto <bastian.soto.j@usach.cl>
2022-01-01 11:58:43 -0300

    [Bastián Soto] - Modelo + migración "Ranking" + atributos

---
+++ database/migrations/2022_01_01_145535_create_rankings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRankingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rankings', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_juego',140);
+            $table->string('género',30);
+            $table->integer('precio');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rankings');
+    }
+}
