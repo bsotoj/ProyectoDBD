b0bd58c53512a94ad5f477a8e5103f4fc2b855f0
Bastian Soto <bastian.soto.j@usach.cl>
2022-01-01 18:54:51 -0300

    [Bastián Soto] - tabla intermedia "Usuario_Comunidad"

---
+++ database/migrations/2022_01_01_215316_create_usuario__comunidads_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuarioComunidadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario__comunidads', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_comunidad')->nullable();
+            $table->foreign('id_comunidad')->references('id')->on('communities');
+
+            $table->unsignedBigInteger('id_usuario')->nullable();
+            $table->foreign('id_usuario')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario__comunidads');
+    }
+}
