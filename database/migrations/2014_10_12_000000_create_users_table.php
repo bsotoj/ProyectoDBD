4e26f3db6ea4d23e132e9e44b05d293431b0a0f9
Bastian Soto <bastian.soto.j@usach.cl>
2022-01-01 12:58:04 -0300

    [Bastián Soto] - tabla "User": atributos + llaves foráneas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,23 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('nombre_usuario');
+            $table->string('contraseña');
+            $table->integer('edad');
+
+            $table->unsignedBigInteger('id_cartera')->nullable();
+            $table->foreign('id_cartera')->references('id')->on('wallets');
+
+            $table->unsignedBigInteger('código_postal')->nullable();
+            $table->foreign('código_postal')->references('id')->on('geozones');
+
+            $table->unsignedBigInteger('id_lista_deseo')->nullable();
+            $table->foreign('id_lista_deseo')->references('id')->on('wish_lists');
+
+
+
+
+           
         });
     }
 
