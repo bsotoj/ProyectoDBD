fb93938e393cc350c05c5e391c8beda5d787c5f9
Bastian Soto <bastian.soto.j@usach.cl>
2022-01-01 18:43:55 -0300

    [Bastián Soto] - Modelo + migración "Sale" + atributos

---
+++ database/migrations/2022_01_01_213853_create_sales_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps('fecha_venta',$precision = 0);
+            $table->integer('monto');
+            $table->integer('descuento');
+
+            $table->unsignedBigInteger('id_usuario')->nullable();
+            $table->foreign('id_usuario')->references('id')->on('users');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
