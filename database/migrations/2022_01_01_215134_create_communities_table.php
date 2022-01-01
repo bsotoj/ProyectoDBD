535b19c9298f04e16a4c1e584d8314f5490866c8
Bastian Soto <bastian.soto.j@usach.cl>
2022-01-01 18:52:54 -0300

    [Bastián Soto] - Modelo + migración "Community" + atributos

---
+++ database/migrations/2022_01_01_215134_create_communities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommunitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('communities', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_comunidad',70);
+
+            $table->unsignedBigInteger('id_juego')->nullable();
+            $table->foreign('id_juego')->references('id')->on('games');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('communities');
+    }
+}
