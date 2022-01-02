901b6b3610229f50f47a5a900a68183ff2741ff6
José Llancamil <jose.llancamil@usach.cl>
2022-01-02 03:43:43 -0300

    [José Llancamil] - Relación entre modelo user-wallet y user-geozone

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function wallet()
+    {
+    return $this->belongsTo(Wallet::class);
+    }
+
+    public function geozone()
+    {
+    return $this->belongsTo('App\Models\Geozone');
+    }
 }
