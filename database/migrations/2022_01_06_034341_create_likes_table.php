24de70b7ecdaab5b12cd7bd3570c5d1ad3179bca
Luis <luis.gonzalez.gr@usach.cl>
2022-01-06 00:45:48 -0300

    [Luis González] - Migración + Modelo Like y llave foranea de comentario

--- database/migrations/2022_01_06_034341_create_likes_table.php
+++ database/migrations/2022_01_06_034341_create_likes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateComentariosTable extends Migration
+class CreateLikesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,17 @@ class CreateComentariosTable extends Migration
      */
     public function up()
     {
-        Schema::create('comentarios', function (Blueprint $table) {
+        Schema::create('likes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('mensaje');
+
 
             $table->unsignedBigInteger('idJuego')->nullable();
             $table->foreign('idJuego')->references('id')->on('juegos');
 
             $table->unsignedBigInteger('idUsuario')->nullable();
             $table->foreign('idUsuario')->references('id')->on('usuarios');
-            
+
         });
     }
 
@@ -34,6 +34,6 @@ class CreateComentariosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comentarios');
+        Schema::dropIfExists('likes');
     }
 }
