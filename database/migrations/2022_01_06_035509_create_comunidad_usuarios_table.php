53032052ebfc2b5ce11a2aa3bbd22113441bcc06
Bastian Soto <bastian.soto.j@usach.cl>
2022-01-06 01:00:19 -0300

    [Bastián Soto] - migración ComunidadUsuario

---
+++ database/migrations/2022_01_06_035509_create_comunidad_usuarios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComunidadUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comunidad_usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('idUsuario')->nullable();
+            $table->foreign('idUsuario')->references('id')->on('usuarios');
+            $table->unsignedBigInteger('idComunidad')->nullable();
+            $table->foreign('idComunidad')->references('id')->on('comunidads');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comunidad_usuarios');
+    }
+}
