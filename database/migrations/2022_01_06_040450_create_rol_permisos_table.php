5e07a7f44c41dd848c526308abfe870c4880926d
Luis <luis.gonzalez.gr@usach.cl>
2022-01-06 01:07:56 -0300

    [Luis González] - Migración + Modelo RolPermiso

---
+++ database/migrations/2022_01_06_040450_create_rol_permisos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolPermisosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rol_permisos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->unsignedBigInteger('idRol')->nullable();
+            $table->foreign('idRol')->references('id')->on('rols');
+
+            $table->unsignedBigInteger('idPermiso')->nullable();
+            $table->foreign('idPermiso')->references('id')->on('permisos');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rol_permisos');
+    }
+}
