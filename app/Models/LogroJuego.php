21158928cb617b6b9cac89d755ed68b0593a2443
Bastian Soto <bastian.soto.j@usach.cl>
2022-01-06 00:43:24 -0300

    [Bastián Soto]  - migración + modelo LogroJuego

---
+++ app/Models/LogroJuego.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LogroJuego extends Model
+{
+    use HasFactory;
+}
