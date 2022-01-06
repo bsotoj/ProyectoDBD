ac7fb0c22d13109fb99b8453670947db7bd61f8b
Luis <luis.gonzalez.gr@usach.cl>
2022-01-06 00:59:51 -0300

    [Luis González] - Migración + Modelo Publicacion

---
+++ database/migrations/2022_01_06_035520_create_publicacions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePublicacionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('publicacions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nombrePublicacion');
+            $table->string('mensajePublicacion');
+
+            $table->unsignedBigInteger('idComunidad')->nullable();
+            $table->foreign('idComunidad')->references('id')->on('comunidads');
+
+            $table->unsignedBigInteger('idUsuario')->nullable();
+            $table->foreign('idUsuario')->references('id')->on('usuarios');
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('publicacions');
+    }
+}
