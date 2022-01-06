35a4603e3ef2e992f2797f84b59e660f903b1216
Luis <luis.gonzalez.gr@usach.cl>
2022-01-06 00:52:39 -0300

    [Luis González] - Migración + Modelo ListaDeseosJuegos

---
+++ database/migrations/2022_01_06_035000_create_lista_deseos_juegos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateListaDeseosJuegosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lista_deseos_juegos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+           
+
+            $table->unsignedBigInteger('idListaDeseo')->nullable();
+            $table->foreign('idListaDeseo')->references('id')->on('lista_deseos');
+
+            $table->unsignedBigInteger('idJuego')->nullable();
+            $table->foreign('idJuego')->references('id')->on('juegos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lista_deseos_juegos');
+    }
+}
