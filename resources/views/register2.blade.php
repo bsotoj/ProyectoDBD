7bba8f0427ffec16046f3636d074203927c5bb56
Bastian Soto <bastian.soto.j@usach.cl>
2022-01-19 09:28:12 -0300

    [Bastián Soto] - Dummy value

--- resources/views/register2.blade.php
+++ resources/views/register2.blade.php
@@ -25,75 +25,45 @@
         <form action="{{route('crearusuario')}}" method="post">
 
             <div class="txt_field">
-<<<<<<< HEAD
                 <input type="text" required name="nombre">
-=======
-                <input type="text" id="nombre" name="nombre" value="" required>
->>>>>>> main
                 <label>Nombre</label>
             </div>
 
 
             <div class="txt_field">
-<<<<<<< HEAD
                 <input type="text" required name="nombreUsuario">
-=======
-                <input type="text" id="username" name="nombreUsuario" value="" required>
->>>>>>> main
                 <label>Usuario</label>
             </div>
 
             <div class="txt_field">
-<<<<<<< HEAD
                 <input type="password" required name="contraseña">
                 <label>Contraseña</label>
             </div>
 
             <div class="txt_field">
                 <input type="email" required name="email">
-=======
-                <input type="password" id="password" name="contraseña" value="" required>
-                <label>Contraseña</label>
-            </div>
-
-
-            <div class="txt_field">
-                <input type="email" id="email" name="email" value="" required>
->>>>>>> main
                 <label>Correo electrónico</label>
             </div>
 
             
             <div class="txt_field">
                 <h5>Fecha de nacimiento</h5>
-<<<<<<< HEAD
                 <input type="date" required name="fechaNacimiento">
-=======
-                <input type="date" id="fechaNacimiento" name="fechaNacimiento" value=""required>
->>>>>>> main
             </div>
 
             
             <div class="txt_field">
-<<<<<<< HEAD
                 <input type="text" required name="nombreLista">
-=======
-                <input type="text" id="nombreLista" name="nombreLista" value="" required>
->>>>>>> main
                 <label>Nombre lista de deseos</label>
             </div>
 
 
                
             <div class="txt_field">
-<<<<<<< HEAD
                 <input type="text" required name="metodoRecarga">  
-=======
-                <input type="text" id="metodoRecarga" name="metodoRecarga" value=""required>
->>>>>>> main
                 <label>Forma de pago</label>
             </div>
-
+            a
             <div class="txt_field">
                 <h5>Región</h5>
                 <select name="format" id="format"name="idRegion" id="idRegion">
