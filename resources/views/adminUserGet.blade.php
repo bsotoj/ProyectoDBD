98f5696d2be3d50b7fe7ffc71a7d30c3d3a278bd
José Llancamil <jose.llancamil@usach.cl>
2022-01-20 16:43:57 -0300

    [José Llancamil] - Fix admin show user view

--- resources/views/adminUserGet.blade.php
+++ resources/views/adminUserGet.blade.php
@@ -13,6 +13,17 @@
 </head>
 
 <body>
+    <header>
+        <div class="nav">
+            <img src="{{ asset('images/logo2.png')}}" alt="" class="logo">
+            <a class="debede" href="#">DEBEDE</a>
+            <ul class="nav-list">
+                <li><i class="fas fa-shopping-cart"></i><a href="#">Carrito</a></li>
+                <li><i class="far fa-heart"></i><a href="#">Lista de deseos</a></li>
+                <li><i class="far fa-user"></i></i><a href='/redirigirPerfil/Id={{$admin['id']}}' class="profile">Mi cuenta</a></li>
+            </ul>
+        </div>
+    </header>
     <main>
         <section class="glass">
             <div class="games">
@@ -31,7 +42,7 @@
                 </div>
                 <div class="links">
                     <a href='/redirigirPerfil/Id={{$admin['id']}}' class="profile">
-                        <h3><i class="far fa-user"></i>Volver al perfil</h3>
+                        <h2><i class="far fa-user"></i>Volver al perfil</h2>
                     </a>
                 </div>
         </section>
